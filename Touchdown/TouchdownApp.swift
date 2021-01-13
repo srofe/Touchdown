d5652168bc7171a86e79cde9006ca7eb7a3d2838
Simon Rofe <simon.rofe@airbus.com>
2021-01-14 08:50:47 +1000

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Simon Rofe on 14/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
