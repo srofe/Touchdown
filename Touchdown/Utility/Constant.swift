59a07e957ba07d17ab37341674a043566e2c75a2
Simon Rofe <simon.rofe@airbus.com>
2021-01-14 09:14:39 +1000

    Add some colour constants

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Simon Rofe on 14/1/21.
+//
+
+import SwiftUI
+
+let colourBackground: Color = Color("ColorBackground")
+let colourGrey: Color = Color(UIColor.systemGray4)
