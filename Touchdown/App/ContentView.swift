decb99647aa9463fa103c14b26629e49d1863380
Simon Rofe <simon.rofe@ieee.org>
2021-01-15 22:33:03 +1000

    Add navigation bar to content view

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 0) {
+            NavigationBarView()
+                .padding()
+                .background(Color.white)
+                .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
             Spacer()
             FooterView()
                 .padding(.horizontal)
