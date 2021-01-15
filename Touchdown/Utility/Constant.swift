92de720abf783bc8107da478bfebaa58af5c297c
Simon Rofe <simon.rofe@ieee.org>
2021-01-16 08:05:10 +1000

    Add constant for categories read from Bundle

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 let players: [Player] = Bundle.main.decode("player.json")
+let categories: [Categoty] = Bundle.main.decode("category.json")
 
 let colourBackground: Color = Color("ColorBackground")
 let colourGrey: Color = Color(UIColor.systemGray4)
