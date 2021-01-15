745f1b9fa148d3cc2b17b587b2b7ab5234aff7c3
Simon Rofe <simon.rofe@ieee.org>
2021-01-16 08:04:04 +1000

    Add category model

---
+++ Touchdown/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Touchdown
+//
+//  Created by Simon Rofe on 16/1/21.
+//
+
+import Foundation
+
+struct Categoty: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
