24f7378c67f492d5a34cdf0074eed5eb3b5d2b68
Simon Rofe <simon.rofe@ieee.org>
2021-01-15 22:54:27 +1000

    Add constant for players read from Bundle

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -7,5 +7,7 @@
 
 import SwiftUI
 
+let players: [Player] = Bundle.main.decode("player.json")
+
 let colourBackground: Color = Color("ColorBackground")
 let colourGrey: Color = Color(UIColor.systemGray4)
