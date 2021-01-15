375b3f6f9d384672fa3f9f043effeb86d968b930
Simon Rofe <simon.rofe@ieee.org>
2021-01-15 22:39:46 +1000

    Correct padding for navigation bar view
    This provides padding to the top of the havigation bar view which is
    based on the safe area insets. This ensures the navitation bar is shown
    correctly on phones with and without the notch at the top of the
    screen.

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -12,7 +12,9 @@ struct ContentView: View {
         ZStack {
             VStack(spacing: 0) {
                 NavigationBarView()
-                    .padding()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
                 Spacer()
