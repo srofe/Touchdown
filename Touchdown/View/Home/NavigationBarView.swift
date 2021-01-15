ff8cf3c5ef4a2b38e1782d6a1df376976aae94f9
Simon Rofe <simon.rofe@ieee.org>
2021-01-15 22:19:02 +1000

    Add red circle overlay to show items in cart

--- Touchdown/View/Home/NavigationBarView.swift
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -17,9 +17,15 @@ struct NavigationBarView: View {
             }
             Spacer()
             Button(action: {}) {
-                Image(systemName: "cart")
-                    .font(.title)
-                    .foregroundColor(.black)
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
             }
         }
     }
