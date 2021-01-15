1971725e33f09dc7d660bd7ea9b4c07c5a145600
Simon Rofe <simon.rofe@ieee.org>
2021-01-15 22:47:19 +1000

    Addd player model type

---
+++ Touchdown/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  Touchdown
+//
+//  Created by Simon Rofe on 15/1/21.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
