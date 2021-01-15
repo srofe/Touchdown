089ad89fbdbcf43c549f69e28acc9d2814f791f0
Simon Rofe <simon.rofe@ieee.org>
2021-01-15 23:06:46 +1000

    Add featured tab view to content view

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -17,9 +17,14 @@ struct ContentView: View {
                     .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
-                Spacer()
-                FooterView()
-                    .padding(.horizontal)
+                ScrollView(.vertical, showsIndicators: false) {
+                    FeateredTabView()
+                        .padding(.vertical, 20)
+                    VStack(spacing: 10) {
+                        FooterView()
+                            .padding(.horizontal)
+                    }
+                }
             }
             .background(colourBackground.ignoresSafeArea(.all, edges: .all))
         }
