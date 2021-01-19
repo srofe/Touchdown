4426f5a822519a3596c261908878057ac9a1da82
Simon Rofe <simon.rofe@airbus.com>
2021-01-19 14:44:54 +1000

    Add products constant

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -10,6 +10,7 @@ import SwiftUI
 // MARK:- Data from Bundle
 let players: [Player] = Bundle.main.decode("player.json")
 let categories: [Category] = Bundle.main.decode("category.json")
+let products: [Product] = Bundle.main.decode("product.json")
 
 // MARK:- Colours
 let colourBackground: Color = Color("ColorBackground")
