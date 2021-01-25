769941e553cc7ad19262d9dffc81c03558d81ea7
Simon Rofe <simon.rofe@ieee.org>
2021-01-25 22:09:18 +1000

    Add start of product detail view

---
+++ Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,31 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by Simon Rofe on 25/1/21.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(sampleProduct.name)
+            Spacer()
+        }
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+            Color(
+                red: sampleProduct.red,
+                green: sampleProduct.green,
+                blue: sampleProduct.blue)
+            ).ignoresSafeArea(.all, edges: .all)
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+            .previewLayout(.fixed(width: 375, height: 812))
+    }
+}
