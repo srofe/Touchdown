a66984b3899e8b8b3d5e38611ee5d1c50441a0f0
Simon Rofe <simon.rofe@ieee.org>
2021-01-25 21:49:59 +1000

    Add a Brand model

---
+++ Touchdown/Model/Brand.swift
@@ -0,0 +1,13 @@
+//
+//  Brand.swift
+//  Touchdown
+//
+//  Created by Simon Rofe on 25/1/21.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
