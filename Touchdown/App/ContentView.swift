a8d073de913dc3f87ec5b6dc8f7183a4547f683e
Simon Rofe <simon.rofe@ieee.org>
2021-01-25 22:00:51 +1000

    Add grid view to content view

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -29,6 +29,8 @@ struct ContentView: View {
                             }
                         }
                         .padding(15)
+                        TitleView(title: "Brands")
+                        BrandGridView()
                         FooterView()
                             .padding(.horizontal)
                     }
