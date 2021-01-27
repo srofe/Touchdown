edb814bc6955e1e57ab76f29c0c3f60d5ca6e557
Simon Rofe <simon.rofe@airbus.com>
2021-01-27 13:26:45 +1000

    Add animation to items in view

--- Touchdown/View/Detail/TopPartDetailView.swift
+++ Touchdown/View/Detail/TopPartDetailView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct TopPartDetailView: View {
+    @State private var isAnimating: Bool = false
+
     var body: some View {
         HStack(alignment: .center, spacing: 6) {
             VStack(alignment: .leading, spacing: 6) {
@@ -18,11 +20,18 @@ struct TopPartDetailView: View {
                     .fontWeight(.black)
                     .scaleEffect(1.35, anchor: .leading)
             }
+            .offset(y: isAnimating ? -50 : -75)
             Spacer()
             Image(sampleProduct.image)
                 .resizable()
                 .scaledToFit()
+                .offset(y: isAnimating ? 0 : -35)
         }
+        .onAppear(perform: {
+            withAnimation(.easeOut(duration: 0.75)) {
+                isAnimating.toggle()
+            }
+        })
     }
 }
 
